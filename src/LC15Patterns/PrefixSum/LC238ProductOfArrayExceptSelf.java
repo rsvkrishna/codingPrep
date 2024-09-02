5f66226f158374f45672c064f96900331ac2f7ca
rsvkrishna <rsaivijaykrishna@gmail.com>
2024-09-02 10:04:37 +0530

    PrefixSum Concept

---
+++ src/LC15Patterns/PrefixSum/LC238ProductOfArrayExceptSelf.java
@@ -0,0 +1,37 @@
+package LC15Patterns.PrefixSum;
+
+import java.util.Arrays;
+
+public class LC238ProductOfArrayExceptSelf {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        System.out.println("arr "+ Arrays.toString(arr));
+        System.out.println(Arrays.toString(productExceptSelf(arr)));
+
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] preFixArr = new int[nums.length];
+        int[] postFixArr = new int[nums.length];
+
+        preFixArr[0] = 1;
+        for(int i =1;i< nums.length;i++){
+            preFixArr[i] = nums[i-1] * preFixArr[i-1];
+        }
+        postFixArr[nums.length-1] = 1;
+
+        for(int i = nums.length-2; i>=0;i--){
+            postFixArr[i] = nums[i+1] * postFixArr[i+1];
+        }
+
+        for(int i =0;i< preFixArr.length;i++){
+            nums[i] = preFixArr[i] * postFixArr[i];
+        }
+
+
+        System.out.println("preFixArr "+ Arrays.toString(preFixArr));
+        System.out.println("postFixArr "+ Arrays.toString(postFixArr));
+
+        return nums;
+    }
+}
---
+++ src/LC15Patterns/PrefixSum/LC560SubArraySumEqualsK.java
@@ -0,0 +1,27 @@
+package LC15Patterns.PrefixSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC560SubArraySumEqualsK {
+    public static void main(String[] args) {
+        System.out.println(subArraySum(new int[]{1,1,1},2));
+        System.out.println(subArraySum(new int[]{3,1,2,4},6));
+
+    }
+
+    public static int subArraySum(int[] nums, int k) {
+        Map<Integer,Integer> m = new HashMap<>();
+        int sum = 0, count =0;
+        m.put(0,1);
+        for(int i =0;i< nums.length;i++){
+            sum += nums[i];
+
+            count += m.getOrDefault(sum-k,0);
+            m.put(sum,m.getOrDefault(sum,0)+1);
+
+        }
+
+        return count;
+    }
+}
