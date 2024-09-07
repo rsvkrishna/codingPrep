c6b2f0a45ea7d6867a9cd1c228f03ed270f2a80a
rsvkrishna <rsaivijaykrishna@gmail.com>
2024-09-08 00:30:40 +0530

    Two Pointers

---
+++ src/LC15Patterns/TwoPointers/LC11ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+package LC15Patterns.TwoPointers;
+
+public class LC11ContainerWithMostWater {
+    public static void main(String[] args) {
+
+        int[] arr={1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(arr));
+
+    }
+
+    public static int maxArea(int[] height) {
+
+        int i=0;int j= height.length-1;
+        int maxVol =0;
+
+        while(i<j){
+            maxVol = Math.max(maxVol,(j-i)*Math.min(height[i],height[j]));
+            if(height[i]<height[j])
+                i++;
+            else
+                j--;
+        }
+
+        return maxVol;
+    }
+}
