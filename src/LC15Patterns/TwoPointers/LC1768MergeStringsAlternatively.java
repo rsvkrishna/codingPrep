0e738895601414f6f0d611434e00c1295b36578f
rsvkrishna <rsaivijaykrishna@gmail.com>
2024-09-11 08:44:49 +0530

    Two Pointers

---
+++ src/LC15Patterns/TwoPointers/LC1768MergeStringsAlternatively.java
@@ -0,0 +1,24 @@
+package LC15Patterns.TwoPointers;
+
+public class LC1768MergeStringsAlternatively {
+    public static void main(String[] args) {
+        String word1 = "abc", word2 = "pqr";
+        System.out.println(mergeAlternately(word1,word2));
+
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder newStr = new StringBuilder();
+        int m = word1.length();
+        int n = word2.length();
+
+        for(int i =0;i< m || i <n; i++){
+            if(i<m)
+                newStr.append(word1.charAt(i));
+            if(i<n)
+                newStr.append(word2.charAt(i));
+        }
+
+        return newStr.toString();
+    }
+}
---
+++ src/LC15Patterns/TwoPointers/LC345ReverseVowelsOfString.java
@@ -0,0 +1,38 @@
+package LC15Patterns.TwoPointers;
+
+public class LC345ReverseVowelsOfString {
+    public static void main(String[] args) {
+        String s = "hello";
+        System.out.println(reverseVowels(s));
+
+    }
+
+    public static String reverseVowels(String s) {
+
+        boolean[] isVowels = new boolean[128];
+        for(char c : "aeiouAEIOU".toCharArray())
+            isVowels[c]=true;
+
+        char[] characters = s.toCharArray();
+        int start=0,end=s.length()-1;
+
+        while (start < end){
+            while (start < end && !isVowels[characters[start]])
+                start++;
+            while(start < end && !isVowels[characters[end]])
+                end--;
+
+            if(start < end){
+                char temp = characters[start];
+                characters[start]=characters[end];
+                characters[end]=temp;
+
+                start++;
+                end--;
+            }
+        }
+
+        return String.valueOf(characters);
+    }
+
+}
