ece341caa8c07ecd062cd5ffd1c6f4fd39c85531
rsvkrishna <rsaivijaykrishna@gmail.com>
2024-11-05 08:28:47 +0530

    FastSlow Pointers

--- src/LC15Patterns/_04FastSlowPointers/LC287DuplicateNumber.java
+++ src/LC15Patterns/_04FastSlowPointers/LC287DuplicateNumber.java
@@ -7,13 +7,17 @@ public class LC287DuplicateNumber {
     }
 
     public static int findDuplicate(int[] nums) {
-        int slow = 0;
-        int fast = 0;
-        do {
+        // Initialize the slow and fast pointers
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+
+        // Find the intersection point of the two runners
+        while (slow != fast) {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        } while (slow != fast);
+        }
 
+        // Find the "entrance" to the cycle
         slow = 0;
         while (slow != fast) {
             slow = nums[slow];
