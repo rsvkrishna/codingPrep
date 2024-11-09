d0c7d743384d40778ed5b4c03f0bd0ade50a2f6b
rsvkrishna <rsaivijaykrishna@gmail.com>
2024-11-09 07:37:32 +0530

    HappyNumber

---
+++ src/Leetcode75/arraysStrings/HappyNumber.java
@@ -0,0 +1,32 @@
+package Leetcode75.arraysStrings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+
+    }
+
+    public static boolean isHappy(int n) {
+        Set<Integer> seenNumbers = new HashSet<>();
+
+        while (n != 1 && !seenNumbers.contains(n)) {
+            seenNumbers.add(n);
+            n = getSumOfSquares(n);
+        }
+
+        return n == 1;
+    }
+
+    private static int getSumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
