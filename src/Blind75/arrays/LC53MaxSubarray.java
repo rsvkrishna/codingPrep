9d9ccfa90aaaaf6610f8507ed641a4324372c166
rsvkrishna <rsaivijaykrishna@gmail.com>
2024-11-14 07:12:35 +0530

    LC53MaxSubarray

--- src/Blind75/arrays/LC53MaxSubarray.java
+++ src/Blind75/arrays/LC53MaxSubarray.java
@@ -2,7 +2,8 @@ package Blind75.arrays;
 
 public class LC53MaxSubarray {
     public static void main(String[] args) {
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        //int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] nums = {-2,-1,-3,-4};
         System.out.println(maxSubArray(nums));
     }
 
